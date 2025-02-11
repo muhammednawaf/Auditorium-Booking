67ed1362e091a50a4f1077d4ddf23686a9f410ea
Muhammed Nawaf M V <111062573+muhammednawaf@users.noreply.github.com>
2025-02-11 07:48:44 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ const state = {
 
 // Function to connect to the database
 module.exports.connect = (done) => {
-    const url = 'mongodb+srv://muhammednawafbuissness:WEPrNexnl2CNM1Jf@cluster0.cnkil.mongodb.net/Auditorium-Bookings?retryWrites=true&w=majority';
+    const url = 'mongodb://muhammednawafbuissness:WEPrNexnl2CNM1Jf@cluster0.cnkil.mongodb.net/Auditorium-Bookings?retryWrites=true&w=majority';
 
     const client = new MongoClient(url, {
         tls: false, // Force TLS
